d02a578a7dcf7a22f2512630888ce7eba76e23aa
Laurent Leseigneur <laurent.leseigneur@bonitasoft.com>
2015-09-07 14:41:10 +0200

    add test

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -1,17 +1,18 @@
 package org.example;
 
-import org.junit.Test;
-
 import static org.example.Main.main;
+
+import org.junit.Test;
 
 /**
  * @author laurentleseigneur
  */
 public class MainTest {
+
     @Test
     public void should_do_the_job() throws Exception {
         //given
-        String[] args = {"HumanTalks"};
+        String[] args = { "HumanTalks" };
 
         //when
         main(args);
@@ -24,7 +25,7 @@ public class MainTest {
     @Test
     public void should_also_do_the_job() throws Exception {
         //given
-        String[] args = {"Human", "Talks"};
+        String[] args = { "Human Talks" };
 
         //when
         main(args);
@@ -34,6 +35,18 @@ public class MainTest {
 
     }
 
+    @Test(expected = IllegalArgumentException.class)
+    public void should_raise_exception() throws Exception {
+        //given
+        String[] args = { "Human", "Talks" };
+
+        //when
+        main(args);
+
+        //then
+        //...how to check error message contains "too many parameters provided!" ???
+
+    }
 
     @Test
     public void should_not_do_the_job_with_null_parameter() throws Exception {
@@ -46,15 +59,12 @@ public class MainTest {
     }
 
     @Test
-    public void should_not_do_the_job_with_empty_parameter() throws Exception {
-        //given
-        String[] args = {};
-
+    public void should_display_usage() throws Exception {
         //when
         main(null);
 
         //then
-        //...how to check result ???
+        //...how to assert usage is printed ???
 
     }
 }
